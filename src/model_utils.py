81326b583b12aafc426bfeec705bfb1eb2e9b9b8
Felipe Perez <perezmaf@myumanitoba.ca>
2024-11-05 19:59:17 -0600

    Add option for deshifting output

--- src/model_utils.py
+++ src/model_utils.py
@@ -46,6 +46,27 @@ def get_model(config, mode="semantic"):
                 pad_value=config.pad_value,
                 padding_mode=config.padding_mode,
             )
+        elif config.model == "utaeshiftout":
+            model = utae.UTAE(
+                input_dim=config.input_dim,
+                encoder_widths=config.encoder_widths,
+                decoder_widths=config.decoder_widths,
+                out_conv=config.out_conv,
+                str_conv_k=config.str_conv_k,
+                str_conv_s=config.str_conv_s,
+                str_conv_p=config.str_conv_p,
+                agg_mode=config.agg_mode,
+                encoder_norm=config.encoder_norm,
+                n_head=config.n_head,
+                d_model=config.d_model,
+                d_k=config.d_k,
+                shift_input=False,
+                shift_output=True,
+                encoder=False,
+                return_maps=False,
+                pad_value=config.pad_value,
+                padding_mode=config.padding_mode,
+            )
         elif config.model == "unet3d":
             model = unet3d.UNet3D(
                 in_channel=10, n_classes=config.num_classes, pad_value=config.pad_value
