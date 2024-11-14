393f51c18c7db424f8731069ca450678679e0bf8
Felipe Perez <perezmaf@myumanitoba.ca>
2024-11-14 02:12:26 -0600

    Create base class for shifting network

--- src/backbones/utae.py
+++ src/backbones/utae.py
@@ -439,34 +439,15 @@ class Temporal_Aggregator(nn.Module):
                 return x.mean(dim=1)
 
 
-class ShiftSqueezeNet(nn.Module):
+class BaseShiftNet(nn.Module):
 
-    def __init__(self, dropout=0.5, ref_day=182, pad_value=None):
-        super(ShiftSqueezeNet, self).__init__()
+    def __init__(self, ref_day=182, interpolation_mode='bilinear', padding_mode='zeros', pad_value=None):
+        super(BaseShiftNet, self).__init__()
 
         self.ref_day = ref_day
+        self.interpolation_mode = interpolation_mode
+        self.padding_mode = padding_mode
         self.pad_value = pad_value
-
-        squeezenet = models.squeezenet1_1(weights=models.SqueezeNet1_1_Weights.DEFAULT)
-        squeezenet.features[0] = nn.Conv2d(2, 64, kernel_size=3, stride=2)
-
-        self.features = squeezenet.features
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Conv2d(512, 2, kernel_size=1, bias=False),
-            nn.ReLU(inplace=True),
-            nn.AdaptiveAvgPool2d(1)
-        )
-
-        _ = nn.init.kaiming_normal_(self.features[0].weight, mode="fan_out", nonlinearity="relu")
-        _ = self.classifier[1].weight.data.zero_()
-
-    def forward(self, x):
-
-        x = self.features(x)
-        x = self.classifier(x)
-
-        return x
 
     def smart_forward_input(self, x, dates):
 
@@ -546,29 +527,44 @@ class ShiftSqueezeNet(nn.Module):
         warp_matrix = torch.hstack((warp_matrix, thetas.reshape(2 * n, 1))).reshape(-1, 2, 3)
 
         grid = F.affine_grid(warp_matrix, images.size(), align_corners=False)
-        new_images = F.grid_sample(images, grid, align_corners=False, mode='bilinear', padding_mode='zeros')
-
+        new_images = F.grid_sample(images, grid, align_corners=False, mode=self.interpolation_mode,
+                                   padding_mode=self.padding_mode)
         if output_range is not None:
             new_images = torch.clamp(new_images, min=output_range[0], max=output_range[1])
 
         return new_images
 
 
-class ShiftResNet18(nn.Module):
+class ShiftSqueezeNet(BaseShiftNet):
+
+    def __init__(self, dropout=0.5, ref_day=182, pad_value=None):
+        super(ShiftSqueezeNet, self).__init__(ref_day=ref_day, pad_value=pad_value)
+
+        squeezenet = models.squeezenet1_1(weights=models.SqueezeNet1_1_Weights.DEFAULT)
+        squeezenet.features[0] = nn.Conv2d(2, 64, kernel_size=3, stride=2)
+
+        self.features = squeezenet.features
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Conv2d(512, 2, kernel_size=1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d(1)
+        )
+
+        nn.init.kaiming_normal_(self.features[0].weight, mode="fan_out", nonlinearity="relu")
+        self.classifier[1].weight.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        return self.classifier(x)
+
+
+class ShiftResNet18(BaseShiftNet):
 
     def __init__(self, backbone='random', ref_day=182, pad_value=None):
-        super(ShiftResNet18, self).__init__()
+        super(ShiftResNet18, self).__init__(ref_day=ref_day, pad_value=pad_value)
 
-        if backbone == 'random':
-            weights = None
-        elif backbone == 'imagenet':
-            weights = models.ResNet18_Weights.DEFAULT
-        else:
-            raise ValueError()
-
-        self.ref_day = ref_day
-        self.pad_value = pad_value
-
+        weights = None if backbone == 'random' else models.ResNet18_Weights.DEFAULT
         resnet = models.resnet18(weights=weights)
 
         self.first_conv = nn.Conv2d(2, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -604,91 +600,6 @@ class ShiftResNet18(nn.Module):
         x = self.fc(x)
 
         return x
-
-    def smart_forward_input(self, x, dates):
-
-        n, t, c, h, w = x.shape
-
-        # Compute the minimum and maximum values across the spatial dimensions (h, w)
-        x_min = x.view(n, t, c, -1).min(dim=3, keepdim=True)[0].view(n, t, c, 1, 1)
-        x_max = x.view(n, t, c, -1).max(dim=3, keepdim=True)[0].view(n, t, c, 1, 1)
-
-        # Apply Min-Max normalization
-        x_norm = (x - x_min) / (x_max - x_min + 1e-8)  # Add epsilon to avoid division by zero
-
-        # # Pick channel (nir)
-        # x_slice = x[:, :, 3:4, :, :]
-        # Pick channel (rgb to grayscale)
-        x_slice = transforms.functional.rgb_to_grayscale(x_norm[:, :, 0:3, :, :])
-        # Select one point along temporal (T) dimension
-        n_indices = torch.arange(x_slice.size(0)).unsqueeze(1)
-        t_indices = torch.argsort(torch.abs(self.ref_day - dates))[:, 0:1]
-        x_ref = x_slice[n_indices, t_indices, :, :, :].expand(-1, t, -1, -1, -1)
-
-        # Concatenate along channels (C) dimension
-        x_pairs = torch.cat((x_slice, x_ref), dim=2)
-
-        if self.pad_value is not None:
-            pad_mask = (x == self.pad_value).all(dim=-1).all(dim=-1).all(dim=-1)
-            if pad_mask.any():
-                # Forward pairs only for non-padded values
-                temp = self.pad_value * torch.ones((n, t, c, h, w), device=x.device, requires_grad=False)
-                thetas = self.forward(x_pairs[~pad_mask])
-                temp[~pad_mask] = self.transform(x[~pad_mask], thetas, output_range=None)
-                out = temp.view(n * t, c, h, w)
-            else:
-                # No padded values found
-                thetas = self.forward(x_pairs.view(n * t, 2, h, w))
-                out = self.transform(x.view(n * t, c, h, w), thetas, output_range=None)
-        else:
-            # No padding has been applied
-            thetas = self.forward(x_pairs.view(n * t, 2, h, w))
-            # if dates is not None:
-            out = self.transform(x.view(n * t, c, h, w), thetas, output_range=None)
-
-        # Retrieve output dimensions since channel (C) height (H) and width (W) might differ after forwarding
-        _, c, h, w = out.shape
-        out = out.view(n, t, c, h, w)
-
-        return out
-
-    def smart_forward_output(self, predictions, masks):
-
-        n, c, h, w = predictions.shape
-
-        x = torch.stack([torch.sigmoid(predictions), masks], dim=1)
-        x = x.view(n, 2 * c, h, w)
-
-        thetas = self.forward(x)
-        predictions_shifted = self.transform(predictions, thetas, output_range=None)
-
-        return predictions_shifted
-
-    def transform(self, images, thetas, output_range=(0.0, 1.0)):
-        """
-        Shifts images by thetas with grid sampling and bilinear interpolation.
-        Args:
-            images : tensor (B, C, H, W), input images
-            thetas : tensor (B, 2), translation params
-            output_range : tuple (min, max), range of output images
-        Returns:
-            out: tensor (B, C, H, W), shifted images
-        """
-
-        n, c, h, w = images.shape
-
-        thetas *= torch.tensor([[-2 / w, -2 / h]], device=thetas.device).repeat(n, 1)
-
-        warp_matrix = torch.tensor([1.0, 0.0, 0.0, 1.0], device=thetas.device).repeat(1, n).reshape(2 * n, 2)
-        warp_matrix = torch.hstack((warp_matrix, thetas.reshape(2 * n, 1))).reshape(-1, 2, 3)
-
-        grid = F.affine_grid(warp_matrix, images.size(), align_corners=False)
-        new_images = F.grid_sample(images, grid, align_corners=False, mode='bilinear', padding_mode='zeros')
-
-        if output_range is not None:
-            new_images = torch.clamp(new_images, min=output_range[0], max=output_range[1])
-
-        return new_images
 
 
 class RecUNet(nn.Module):
